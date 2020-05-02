9e5654c8621d69239d1f49164b00ed75d47db6be
Pedro Valentinuci <pevalentinuci@gmail.com>
2020-05-02 11:17:25 -0300

    identação e criação desafio-2

--- aula-02/desafio-1.rb
+++ aula-02/desafio-1.rb
@@ -1,8 +1,11 @@
 puts "Coloque os seus dados!!"
+
 print "Digite o seu nome: "
     name = gets.chomp
+
 print "Digite sua idade: "
     age = gets.chomp
 
 puts "Este(a) é #{name}, ele(a) tem #{age} anos de idade."
+
 puts "Agradecido pela sua participação"
