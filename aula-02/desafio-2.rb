9070dd2a7a3c06b1f1fd1d9cce9fedf835e6c0cb
Pedro Valentinuci <pevalentinuci@gmail.com>
2020-05-02 11:25:17 -0300

    soma feita

---
+++ aula-02/desafio-2.rb
@@ -0,0 +1,14 @@
+puts "Vamos fazer umas contas!"
+
+print "Digite um número: "
+    number1 = gets.chomp.to_i
+
+print "Digite outro número: "
+    number2 = gets.chomp.to_i
+
+puts "#{number1} + #{number2}"
+puts ""
+puts ""
+puts ""
+puts ""
+puts ""
