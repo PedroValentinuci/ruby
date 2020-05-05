7047167c6adb841358600e5c4ece9767c20b16a5
Pedro Valentinuci <pevalentinuci@gmail.com>
2020-05-05 16:59:21 -0300

    calc2 agora funciona

---
+++ aula-03/desafios/calc2.rb
@@ -0,0 +1,45 @@
+result = ''
+
+loop do
+ puts result 
+ puts 'Selecione uma das seguintes opções'
+ puts '1- Adicionar'
+ puts '2- Subtrair'
+ puts '3- Multiplicar'
+ puts '4- Dividir'
+ puts '5- Potência'
+ puts '6- Módulo (Resto da divisão)'
+ puts '0- Sair'
+ print 'Opção: '
+ 
+ option = gets.chomp.to_i
+ 
+ case option 
+ when 1..5
+   print 'Digite o primeiro número: '
+    number1 = gets.chomp.to_i
+   
+   print 'Digite o segundo número: '
+    number2 = gets.chomp.to_i
+   case option 
+   when 1
+     result = "#{number1} + #{number2} = #{number1 + number2}"
+   when 2
+     result = "#{number1} - #{number2} = #{number1 - number2}"
+   when 3
+     result = "#{number1} * #{number2} = #{number1 * number2}"
+   when 4
+     result = "#{number1} / #{number2} = #{number1 / number2}"
+   when 5
+     result = "#{number1} ** #{number2} = #{number1 ** number2}"
+   when 6 
+     result = "#{number1} % #{number2} = #{number1 % number2}"
+   end
+ when 0
+   break
+ else 
+   result = 'Opção inválida'
+ end
+ # Comando que limpa o console
+ system "clear"
+end
