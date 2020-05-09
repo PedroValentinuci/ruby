c53545e28b353d35df20ec18c0aa0b94a6712bde
Pedro Valentinuci <pevalentinuci@gmail.com>
2020-05-08 23:56:53 -0300

    puts em todas as frases

--- my-things/music-option.rb
+++ my-things/music-option.rb
@@ -1,40 +1,40 @@
 loop do
     puts "************************************"
-    puts " 1- Rap / trap
-    2- Rock
-    3- Pop
-    4- Eletronica
-    5- Indie"
+    puts " 1- Rap / trap"
+    puts "2- Rock"
+    puts "3- Pop"
+    puts "4- Eletronica"
+    puts "5- Indie"
     print "Opção: "
         option = gets.chomp.to_i
 
 case option
     when 1 #Rap / trap
         loop do
-            puts "Escolha:
-            1- Nacional
-            2- Gringa"
+            puts "Escolha:"
+            puts "1- Nacional"
+            puts "2- Gringa"
             print "Opção: "
                 nation1 = gets.chomp.to_i
         
             case nation1
                 when 1 #nacional
-                    puts "Algumas opções:
-                    1- Haikaiss
-                    2- Costa Gold
-                    3- Hungria
-                    4- JayA Luuk
-                    5- Mikezin
-                    6- Matue"
+                    puts "Algumas opções:"
+                    puts "1- Haikaiss"
+                    puts "2- Costa Gold"
+                    puts "3- Hungria"
+                    puts "4- JayA Luuk"
+                    puts "5- Mikezin"
+                    puts "6- Matue"
                     break
                 when 2 #gringa
-                    puts "Algumas opções:
-                    1- Eminem
-                    2- 50 cent
-                    3- Travis Scott
-                    4- Post Malone
-                    5- Drake
-                    6- Kendrick Lamar"
+                    puts "Algumas opções:"
+                    puts "1- Eminem"
+                    puts "2- 50 cent"
+                    puts "3- Travis Scott"
+                    puts "4- Post Malone"
+                    puts "5- Drake"
+                    puts "6- Kendrick Lamar"
                     break
                 else
                     puts "Opção invalida"
@@ -45,30 +45,30 @@ case option
             
     when 2 #Rock
         loop do
-            puts "Escolha:
-            1- Nacional
-            2- Gringa"
+            puts "Escolha:"
+            puts "1- Nacional"
+            puts "2- Gringa"
             print "Opção: "
                 nation2 = gets.chomp.to_i
         
             case nation2
                 when 1 #nacional
-                    puts "Algumas opções:
-                    1- Legião Urbana
-                    2- Charlie Brown Jr.
-                    3- Barão Vermelho
-                    4- Ultraje a rigor
-                    5- Capital inicial
-                    6- Titãs"
+                    puts "Algumas opções:"
+                    puts "1- Legião Urbana"
+                    puts "2- Charlie Brown Jr."
+                    puts "3- Barão Vermelho"
+                    puts "4- Ultraje a rigor"
+                    puts "5- Capital inicial"
+                    puts "6- Titãs"
                     break
                 when 2 #gringa
-                    puts "Algumas opções: 
-                    1- Linkin park
-                    2- Nirvana
-                    3- AC/DC
-                    4- Guns N' Roses
-                    5- Red Hot Chili Peppers
-                    6- Fall Out Boy"
+                    puts "Algumas opções:" 
+                    puts "1- Linkin park"
+                    puts "2- Nirvana"
+                    puts "3- AC/DC"
+                    puts "4- Guns N' Roses"
+                    puts "5- Red Hot Chili Peppers"
+                    puts "6- Fall Out Boy"
                     break
                 else
                     puts "Opção invalida"
@@ -79,30 +79,30 @@ case option
             
     when 3 #Pop
         loop do
-            puts "Escolha:
-            1- Nacional
-            2- Gringa"
+            puts "Escolha:"
+            puts "1- Nacional"
+            puts "2- Gringa"
             print "Opção: "
                 nation3 = gets.chomp.to_i
         
             case nation3
                 when 1 #nacional
-                    puts "Algumas opções:
-                    1- Melim
-                    2- Giulia Be
-                    3- Vitor Kley
-                    4- AnaVitória
-                    5- Vitão
-                    6- Falto gente boa pra completar o ultimo :("
+                    puts "Algumas opções:"
+                    puts "1- Melim"
+                    puts "2- Giulia Be"
+                    puts "3- Vitor Kley"
+                    puts "4- AnaVitória"
+                    puts "5- Vitão"
+                    puts "6- Falto gente boa pra completar o ultimo :("
                     break
                 when 2 #gringa
-                    puts "Algumas opções:
-                    1- Tones And I
-                    2- Dua Lipa
-                    3- Shawn Mendes
-                    4- Camila Cabello
-                    5- Marroon 5
-                    6- Não sei se todos os nomes citados acima são mesmo pop :("
+                    puts "Algumas opções:"
+                    puts "1- Tones And I"
+                    puts "2- Dua Lipa"
+                    puts "3- Shawn Mendes"
+                    puts "4- Camila Cabello"
+                    puts "5- Marroon 5"
+                    puts "6- Não sei se todos os nomes citados acima são mesmo pop :("
                     break
                 else
                     puts "Opção invalida"
@@ -113,30 +113,30 @@ case option
             
     when 4 #eletronica
         loop do
-            puts "Escolha:
-            1- Nacional
-            2- Gringa"
+            puts "Escolha:"
+            puts "1- Nacional"
+            puts "2- Gringa"
             print "Opção: "
                 nation4 = gets.chomp.to_i
         
             case nation4
                 when 1 #nacional
-                    puts "Algumas opções:
-                    1- Alok
-                    2- Baskhar
-                    3- Vintage Culture
-                    4- Kvsh
-                    5- Cat dealers
-                    6- Liu"
+                    puts "Algumas opções:"
+                    puts "1- Alok"
+                    puts "2- Baskhar"
+                    puts "3- Vintage Culture"
+                    puts "4- Kvsh"
+                    puts "5- Cat dealers"
+                    puts "6- Liu"
                     break
                 when 2 #gringa
-                    puts "Algumas opções:
-                    1- Marshmello
-                    2- Alan Walker
-                    3- Mellen Gi & Tommee Profitt
-                    4- Martin Garrix
-                    5-David Guetta
-                    6- Tiësto"
+                    puts "Algumas opções:"
+                    puts "1- Marshmello"
+                    puts "2- Alan Walker"
+                    puts "3- Mellen Gi & Tommee Profitt"
+                    puts "4- Martin Garrix"
+                    puts "5-David Guetta"
+                    puts "6- Tiësto"
                     break
                 else
                     puts "Opção invalida"
@@ -147,9 +147,9 @@ case option
             
     when 5 #Indie
         loop do
-            puts "Escolha:
-            1- Nacional
-            2- Gringa"
+            puts "Escolha:"
+            puts "1- Nacional"
+            puts "2- Gringa"
             print "Opção: "
                 nation5 = gets.chomp.to_i
         
@@ -158,13 +158,13 @@ case option
                     puts "Então meu mano(a), não tem muito não q canta essa para ai mas um dia eu acho"
                     break
                 when 2 #gringa
-                    puts "Algumas opções:
-                    1- The Smiths
-                    2- The National
-                    3- Aurora
-                    4- The Lumineers
-                    5- Billie Elish (não sabia q ela cantava 'indie')
-                    6- N tem mais ninguem pra por na ultima posição :("
+                    puts "Algumas opções:"
+                    puts "1- The Smiths"
+                    puts "2- The National"
+                    puts "3- Aurora"
+                    puts "4- The Lumineers"
+                    puts "5- Billie Elish (não sabia q ela cantava 'indie')"
+                    puts "6- N tem mais ninguem pra por na ultima posição :("
                     break
                 else
                     puts "Opção invalida"
