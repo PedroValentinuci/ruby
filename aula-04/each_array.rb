f78105034c7e921ce20670d56ace9d01d44db00a
Pedro Valentinuci <pevalentinuci@gmail.com>
2020-05-20 16:44:15 -0300

    teste

--- aula-04/each_array.rb
+++ aula-04/each_array.rb
@@ -1,9 +1,11 @@
 names = ['Sophia', 'Joãozinho', 'Joanazinha']
  
 name = 'Pedro Valentinuci'
- 
+
+print "Digite o seu nome: "
+
 names.each do |name|
-    puts name + 'é meu nome!'
+    puts name + ' é meu nome!'
 end
  
 puts "#{name} é o nome do merda que criou isso!!" 
